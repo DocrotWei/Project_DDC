6c9cba25ebeb2fbec89056decf13535530363a2e
XW_Chen <894042825@qq.com>
2022-04-03 20:05:58 +0800

    实现了 文件读取 以及rom的运用

--- sim_1/new/sig_gen_sim.vhd
+++ sim_1/new/sig_gen_sim.vhd
@@ -17,10 +17,26 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+-- USE ieee.Std_logic_unSigned.ALL;
+
+USE ieee.numeric_std.ALL;
+USE std.textio.ALL;
+USE ieee.std_logic_textio.ALL;
 
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_SIGNED.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+library UNISIM;
+use UNISIM.VComponents.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -31,13 +47,199 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity sig_gen_sim is
---  Port ( );
-end sig_gen_sim;
+ENTITY sig_gen_sim IS
 
-architecture Behavioral of sig_gen_sim is
+END sig_gen_sim;
 
-begin
+ARCHITECTURE Behavioral OF sig_gen_sim IS
+  COMPONENT sig_gen_dri IS
+    PORT (
+      I_clk        : IN std_logic;
+      I_rst_n      : IN std_logic;
+      O_data_ori0  : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori1  : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori2  : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori3  : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori4  : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori5  : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori6  : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori7  : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori8  : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori9  : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori10 : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori11 : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori12 : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori13 : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori14 : OUT std_logic_vector(7 DOWNTO 0);
+      O_data_ori15 : OUT std_logic_vector(7 DOWNTO 0)
+    );
+  END COMPONENT;
+  CONSTANT SIM_TIME : integer := 18750;
+
+  SIGNAL S_clk        : std_logic := '0';
+  SIGNAL S_rst_n      : std_logic := '1';
+  SIGNAL S_data_ori0  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori1  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori2  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori3  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori4  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori5  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori6  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori7  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori8  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori9  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori10 : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori11 : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori12 : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori13 : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori14 : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori15 : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_sim_cnt    : std_logic_vector(18 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL sim_end      : std_logic                     := '0';
+  SIGNAL dataOUT      : std_logic                     := '0';
+  CONSTANT CLK_PERIOD : time                          := 5.333 ns;
+  CONSTANT RST_PERIOD : time                          := 53.33 ns;
+
+  SIGNAL rst_n_r    : std_logic := '0';
+  SIGNAL rst_n_rise : std_logic := '0';
+  SIGNAL rst_cnt    : natural   := 0;
+
+  type status_type is (s_idle,  s_write,  s_write_end,  s_read_begin,   s_read,   s_end);
+signal c_status:status_type:=s_idle;
+
+signal cnt_from_file:std_logic_vector(7 downto 0):=(others=>'0');
+
+BEGIN
+
+S_rst_n<='1',
+'0' after 100 ns,
+'1' after 200 ns,
+'0' after 1 us,
+'1' after 1100 ns,
+'0' after 2 us,
+'1' after 2100 ns,
+'0' after 3 us,
+'1' after 3100 ns,
+'0' after 4 us,
+'1' after 4100 ns,
+'0' after 5 us,
+'1' after 5100 ns,
+'0' after 6 us,
+'1' after 6100 ns,
+'0' after 7 us,
+'1' after 7100 ns,
+'0' after 8 us,
+'1' after 8100 ns;
+
+S_clk<=not S_clk after 5.333 ns;
+
+  sig_gen_dri_u : sig_gen_dri
+  PORT MAP(
+    I_clk        => S_clk,
+    I_rst_n      => S_rst_n,
+    O_data_ori0  => S_data_ori0,
+    O_data_ori1  => S_data_ori1,
+    O_data_ori2  => S_data_ori2,
+    O_data_ori3  => S_data_ori3,
+    O_data_ori4  => S_data_ori4,
+    O_data_ori5  => S_data_ori5,
+    O_data_ori6  => S_data_ori6,
+    O_data_ori7  => S_data_ori7,
+    O_data_ori8  => S_data_ori8,
+    O_data_ori9  => S_data_ori9,
+    O_data_ori10 => S_data_ori10,
+    O_data_ori11 => S_data_ori11,
+    O_data_ori12 => S_data_ori12,
+    O_data_ori13 => S_data_ori13,
+    O_data_ori14 => S_data_ori14,
+    O_data_ori15 => S_data_ori15
+  );
 
 
-end Behavioral;
+
+  process(S_clk)
+  begin
+  if rising_edge(S_clk) then
+      rst_n_r<=S_rst_n;
+  else
+  null;
+  end if;
+  end process;
+
+  rst_n_rise<=S_rst_n and (not rst_n_r);
+
+  process(S_clk)
+  begin
+  if rising_edge(S_clk) then
+      if rst_n_rise='1' then
+          rst_cnt<=rst_cnt+1;
+      else
+      null;
+      end if;
+  else
+  null;
+  end if; 
+  end process;
+
+
+  process(S_rst_n, S_clk)
+  file file1:text;
+  variable file_status:file_open_status;
+  variable buf:LINE;
+  --  variable cnt_from_file_r:std_logic_vector(7 downto 0):=(others=>'0');
+  variable cnt_from_file_r:natural:=0;
+  variable file_name:string(1 to 11):="nihaonihaon";
+  constant rst_cnt_stop:natural:=10;
+  begin
+  if rising_edge(S_clk) then
+      case c_status is
+      when s_idle     =>      c_status<=s_write;
+                              file_open(file_status,file1,"data_record0.txt",write_mode);                              
+
+      when s_write    =>      if rst_cnt>=rst_cnt_stop then
+                                  c_status<=s_write_end;                                  
+                              else
+                                  c_status<=s_write;
+                              end if;
+                              write(buf,conv_integer(S_data_ori0));
+                              writeline(file1,buf);
+
+      when s_write_end  =>    file_close(file1);
+                              c_status<=s_end;
+
+      -- when s_read_begin =>    file_open(file_status,file1,"sinWave.txt",read_mode);
+      --                         readline(file1,buf);
+      --                         --read(buf,file_name);
+      --                         writeline(output,buf);
+      --                         c_status<=s_read;
+
+      -- when s_read     =>      
+      --                         if endfile(file1) then
+      --                             c_status<=s_end;
+      --                         else
+      --                             c_status<=s_read;
+      --                         readline(file1,buf);
+      --                         read(buf,cnt_from_file_r);
+      --                         cnt_from_file<=std_logic_vector(to_unsigned(cnt_from_file_r,8));                                    
+      --                         end if;
+
+
+      when s_end      =>      c_status<=s_end;
+                              file_close(file1);
+
+      when others     =>      c_status<=s_end;
+      end case;
+  else
+  null;
+  end if;
+  end process;
+
+
+END Behavioral;
+
+
+
+
+
+
+
--- sources_1/new/sig_gen_dri.vhd
+++ sources_1/new/sig_gen_dri.vhd
@@ -3,57 +3,57 @@
 -- Engineer: 
 -- 
 -- Create Date: 2022/04/01 16:54:42
--- Design Name: 
--- Module Name: sig_gen_dri - Behavioral
+-- DeSign Name: 
+-- Module Name: Sig_gen_dri - Behavioral
 -- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
+-- Target DeviceS: 
+-- Tool VerSionS: 
+-- DeScription: 
 -- 
--- Dependencies: 
+-- DependencieS: 
 -- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
+-- ReviSion:
+-- ReviSion 0.01 - File Created
+-- Additional CommentS:
 -- 
 ----------------------------------------------------------------------------------
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-USE ieee.std_logic_1164.ALL;
-USE ieee.std_logic_unsigned.ALL;
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+USE ieee.Std_logic_1164.ALL;
+USE ieee.Std_logic_unSigned.ALL;
+-- Uncomment the following library declaration if uSing
+-- arithmetic functionS with Signed or UnSigned valueS
+--uSe IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
+-- Uncomment the following library declaration if inStantiating
+-- any Xilinx leaf cellS in thiS code.
 --library UNISIM;
---use UNISIM.VComponents.all;
+--uSe UNISIM.VComponentS.all;
 
-ENTITY sig_gen_dri IS
+ENTITY Sig_gen_dri IS
   PORT (
-    i_clk        : IN std_logic;
-    i_rst_n      : IN std_logic;
-    o_data_ori0  : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori1  : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori2  : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori3  : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori4  : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori5  : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori6  : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori7  : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori8  : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori9  : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori10 : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori11 : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori12 : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori13 : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori14 : OUT std_logic_vector(7 DOWNTO 0);
-    o_data_ori15 : OUT std_logic_vector(7 DOWNTO 0)
+    I_clk        : IN std_logic;
+    I_rst_n      : IN std_logic;
+    O_data_ori0  : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori1  : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori2  : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori3  : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori4  : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori5  : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori6  : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori7  : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori8  : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori9  : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori10 : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori11 : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori12 : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori13 : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori14 : OUT std_logic_vector(7 DOWNTO 0);
+    O_data_ori15 : OUT std_logic_vector(7 DOWNTO 0)
   );
-END sig_gen_dri;
+END Sig_gen_dri;
 
-ARCHITECTURE Behavioral OF sig_gen_dri IS
+ARCHITECTURE Behavioral OF Sig_gen_dri IS
   COMPONENT blk_mem_gen_0
     PORT (
       clka  : IN std_logic;
@@ -65,82 +65,107 @@ ARCHITECTURE Behavioral OF sig_gen_dri IS
     );
   END COMPONENT;
 
-  SIGNAL s_data_ori0  : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori1  : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori2  : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori3  : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori4  : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori5  : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori6  : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori7  : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori8  : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori9  : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori10 : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori11 : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori12 : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori13 : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori14 : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_data_ori15 : std_logic_vector(7 DOWNTO 0);
-  SIGNAL s_mem_addr   : std_logic_vector(15 DOWNTO 0);
-  SIGNAL s_douta_mem  : std_logic_vector(127 DOWNTO 0);
-  SIGNAL s_dina_mem   : std_logic_vector(127 DOWNTO 0);
+  CONSTANT MEM_SIZE : integer := 300000;
+
+  SIGNAL S_data_ori0  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori1  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori2  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori3  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori4  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori5  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori6  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori7  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori8  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori9  : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori10 : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori11 : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori12 : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori13 : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori14 : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_data_ori15 : std_logic_vector(7 DOWNTO 0);
+  SIGNAL S_addr_mem   : std_logic_vector(18 DOWNTO 0):= (others => '0' );
+  SIGNAL S_douta_mem  : std_logic_vector(127 DOWNTO 0);
+  SIGNAL S_dina_mem   : std_logic_vector(7 DOWNTO 0);
 BEGIN
 
-  PROCESS (i_clk, i_rst_n)
-  BEGIN
-    IF i_rst_n = '0' THEN
-      s_mem_addr <= (OTHERS => '0');
-    ELSIF rising_edge(i_clk) THEN
-      s_mem_addr <= s_mem_addr + x"01";
-    END IF;
-  END PROCESS;
 
-  PROCESS (i_clk, i_rst_n)
+  PROCESS (I_clk, I_rst_n)
   BEGIN
-    IF i_rst_n = '0' THEN
-      s_data_ori0  <= (OTHERS => '0');
-      s_data_ori1  <= (OTHERS => '0');
-      s_data_ori2  <= (OTHERS => '0');
-      s_data_ori3  <= (OTHERS => '0');
-      s_data_ori4  <= (OTHERS => '0');
-      s_data_ori5  <= (OTHERS => '0');
-      s_data_ori6  <= (OTHERS => '0');
-      s_data_ori7  <= (OTHERS => '0');
-      s_data_ori8  <= (OTHERS => '0');
-      s_data_ori9  <= (OTHERS => '0');
-      s_data_ori10 <= (OTHERS => '0');
-      s_data_ori11 <= (OTHERS => '0');
-      s_data_ori12 <= (OTHERS => '0');
-      s_data_ori13 <= (OTHERS => '0');
-      s_data_ori14 <= (OTHERS => '0');
-      s_data_ori15 <= (OTHERS => '0');
-    ELSIF rising_edge(i_clk) THEN
-      s_data_ori0  <= s_douta_mem(7 DOWNTO 0);
-      s_data_ori1  <= s_douta_mem(15 DOWNTO 8);
-      s_data_ori2  <= s_douta_mem(23 DOWNTO 16);
-      s_data_ori3  <= s_douta_mem(31 DOWNTO 24);
-      s_data_ori4  <= s_douta_mem(39 DOWNTO 32);
-      s_data_ori5  <= s_douta_mem(47 DOWNTO 40);
-      s_data_ori6  <= s_douta_mem(55 DOWNTO 48);
-      s_data_ori7  <= s_douta_mem(63 DOWNTO 56);
-      s_data_ori8  <= s_douta_mem(71 DOWNTO 64);
-      s_data_ori9  <= s_douta_mem(79 DOWNTO 72);
-      s_data_ori10 <= s_douta_mem(87 DOWNTO 80);
-      s_data_ori11 <= s_douta_mem(95 DOWNTO 88);
-      s_data_ori12 <= s_douta_mem(103 DOWNTO 96);
-      s_data_ori13 <= s_douta_mem(111 DOWNTO 104);
-      s_data_ori14 <= s_douta_mem(119 DOWNTO 112);
-      s_data_ori15 <= s_douta_mem(127 DOWNTO 120);
+    IF I_rst_n = '0' THEN
+      S_data_ori0  <= (OTHERS => '0');
+      S_data_ori1  <= (OTHERS => '0');
+      S_data_ori2  <= (OTHERS => '0');
+      S_data_ori3  <= (OTHERS => '0');
+      S_data_ori4  <= (OTHERS => '0');
+      S_data_ori5  <= (OTHERS => '0');
+      S_data_ori6  <= (OTHERS => '0');
+      S_data_ori7  <= (OTHERS => '0');
+      S_data_ori8  <= (OTHERS => '0');
+      S_data_ori9  <= (OTHERS => '0');
+      S_data_ori10 <= (OTHERS => '0');
+      S_data_ori11 <= (OTHERS => '0');
+      S_data_ori12 <= (OTHERS => '0');
+      S_data_ori13 <= (OTHERS => '0');
+      S_data_ori14 <= (OTHERS => '0');
+      S_data_ori15 <= (OTHERS => '0');
+    ELSIF rising_edge(I_clk) THEN
+      S_data_ori0  <= S_douta_mem(7 DOWNTO 0);
+      S_data_ori1  <= S_douta_mem(15 DOWNTO 8);
+      S_data_ori2  <= S_douta_mem(23 DOWNTO 16);
+      S_data_ori3  <= S_douta_mem(31 DOWNTO 24);
+      S_data_ori4  <= S_douta_mem(39 DOWNTO 32);
+      S_data_ori5  <= S_douta_mem(47 DOWNTO 40);
+      S_data_ori6  <= S_douta_mem(55 DOWNTO 48);
+      S_data_ori7  <= S_douta_mem(63 DOWNTO 56);
+      S_data_ori8  <= S_douta_mem(71 DOWNTO 64);
+      S_data_ori9  <= S_douta_mem(79 DOWNTO 72);
+      S_data_ori10 <= S_douta_mem(87 DOWNTO 80);
+      S_data_ori11 <= S_douta_mem(95 DOWNTO 88);
+      S_data_ori12 <= S_douta_mem(103 DOWNTO 96);
+      S_data_ori13 <= S_douta_mem(111 DOWNTO 104);
+      S_data_ori14 <= S_douta_mem(119 DOWNTO 112);
+      S_data_ori15 <= S_douta_mem(127 DOWNTO 120);
     END IF;
   END PROCESS;
 
   blk_men_gen_0_u : blk_mem_gen_0
   PORT MAP(
-    clka  => i_clk,
+    clka  => I_clk,
     ena   => '1',
     wea   => "0",
-    addra => s_mem_addr,
-    dina  => s_dina_mem,
-    douta => s_douta_mem
+    addra => S_addr_mem,
+    dina  => S_dina_mem,
+    douta => S_douta_mem
   );
+
+  PROCESS (I_clk, I_rst_n)
+  BEGIN
+    IF I_rst_n = '0' THEN
+      S_addr_mem <= (OTHERS => '0');
+    ELSIF rising_edge(I_clk) THEN
+      IF S_addr_mem = MEM_SIZE THEN
+        S_addr_mem <= (OTHERS => '0');
+      ELSE
+        S_addr_mem <= S_addr_mem + x"10";
+      END IF;
+    END IF;
+  END PROCESS;
+
+  O_data_ori0  <= S_data_ori0;
+  O_data_ori1  <= S_data_ori1;
+  O_data_ori2  <= S_data_ori2;
+  O_data_ori3  <= S_data_ori3;
+  O_data_ori4  <= S_data_ori4;
+  O_data_ori5  <= S_data_ori5;
+  O_data_ori6  <= S_data_ori6;
+  O_data_ori7  <= S_data_ori7;
+  O_data_ori8  <= S_data_ori8;
+  O_data_ori9  <= S_data_ori9;
+  O_data_ori10 <= S_data_ori10;
+  O_data_ori11 <= S_data_ori11;
+  O_data_ori12 <= S_data_ori12;
+  O_data_ori13 <= S_data_ori13;
+  O_data_ori14 <= S_data_ori14;
+  O_data_ori15 <= S_data_ori15;
+
 END Behavioral;
